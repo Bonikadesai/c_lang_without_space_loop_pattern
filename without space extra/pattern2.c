d2150ae2095696fb6b413f2f90a7c9e2434c2224
Bonika Desai <114163756+Bonikadesai@users.noreply.github.com>
2023-10-25 22:25:42 +0530

    Extra pattern in without space

---
+++ without space extra/pattern2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+ int i,j;
+  int n=1;
+  char m='A';
+ for(i=1; i<=5; i++)
+ {
+  for(j=1; j<=i; j++)
+  {
+    if(j%2==0)
+       printf(" %c",m++);
+    else
+       printf(" %d",n++);
+  }
+  printf("\n");
+ }
+ 
+ return 0;
+}
